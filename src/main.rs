1677db8dc756ffcd2c1425f6bacca19bce187ffb
oriol <filter.oriol@gmail.com>
2025-04-28 00:00:22 +0200

    working on loading apps/values from the db

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::path::Path;
 use std::process::exit;
 use reqwest;
 use serde::{Deserialize, Serialize};
+use serde_json::{Value};
+
 #[derive(Serialize, Deserialize)]
 struct TAG_INFO {
     url: String,
@@ -22,14 +24,33 @@ struct APP {
     published_at: String
 }
 
-struct APP_JSON {
-    vec: Vec<APP>
+// struct APP_VECTOR {
+//     vec: Vec<APP>
+// }
+
+struct CONFIG{
+    storage_path: String,
+    _db_file_name: String,
+    apps: Vec<APP>
+}
+
+impl CONFIG {
+    fn db_path(&self) -> String {
+        format!("{}/{}",self.storage_path,self._db_file_name)
+    }
 }
 
 fn main() {
     let destination_path: String="/tmp/xrd_mods".to_string();
-    check_db_exists(destination_path.to_owned(),true);
+    let config = CONFIG{
+        storage_path: destination_path,
+        _db_file_name: "db.json".to_string(),
+        apps: vec![]
+    };
+    // load_db(&config);
+    check_db_exists(config.db_path(),true);
 
+    load_apps(&config);
 
     // let xrd_folder: String="/home/goblin/.local/share/Steam/steamapps/common/GUILTY GEAR Xrd -REVELATOR-/".to_string();
 
@@ -38,10 +59,26 @@ fn main() {
     // https://github.com/Iquis/rev2-wakeup-tool/
 
     // download_function_tool()
-    download_hitbox_overlay(destination_path);
+    download_hitbox_overlay(config.storage_path);
 
 }
 
+fn load_apps(config: &CONFIG) -> std::io::Result<()> {
+    let mut file = File::open(config.db_path())?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    // println!("{:?}", contents);
+
+    let value: Value = serde_json::from_str(&*contents)?;
+    println!("{:?}",value);
+
+    Ok(())
+}
+
+// fn load_db(config: &CONFIG){
+//     check_db_exists(config.db_path(),true);
+//     return;
+// }
 
 #[tokio::main]
 async fn get_latest_release(repo_url: String) -> Result<String, reqwest::Error> {
@@ -111,14 +148,13 @@ fn create_new_db(file_path: String) -> std::io::Result<()> {
     Ok(())
 }
 
-fn check_db_exists(destination_path: String, create_db: bool) -> bool {
+fn check_db_exists(db_path: String, create_db: bool) -> bool {
     let mut is_present:bool=false;
-    let db_path=format!("{destination_path}/db.json");
     is_present = Path::new(&db_path).exists();
 
     match (is_present,create_db) {
         (true,false) | (true, true) => {
-            println!("DB found, skipping");
+            println!("DB found");
         }
         (false,true) => {
             println!("DB not found");
